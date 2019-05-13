505187ea5b9c9f1aa5de528fa8301034303e6dbd
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-13 12:30:42 +0800

    Initialize route handler file

---
+++ posts/postsRoutes.js
@@ -0,0 +1,55 @@
+const express = require('express')
+// Import database methods
+const db = require('../data/db')
+// Instantiate router
+const router = express.Router()
+
+// GET
+router.get(`/`, async (req, res) => {
+  try {
+
+  }
+  catch (err) {
+
+  }
+})
+
+router.get(`/:id`, async (req, res) => {
+  try {
+
+  }
+  catch (err) {
+
+  }
+})
+
+// POST
+router.post(`/`, async (req, res) => {
+  try {
+
+  }
+  catch (err) {
+
+  }
+})
+
+// PUT
+router.put(`/`, async (req, res) => {
+  try {
+
+  }
+  catch (err) {
+
+  }
+})
+
+
+// DELETE
+router.delete(`/`, async (req, res) => {
+  try {
+
+  }
+  catch (err) {
+
+  }
+})
