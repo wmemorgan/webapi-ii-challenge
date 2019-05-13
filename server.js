08bd0c3a64df92735d03fe5590c159e8bb61ce0b
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-13 13:00:50 +0800

    Fix JSON parsing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const postsRoutes = require('./posts/postsRoutes')
 
 const server = express()
-server.use(express())
+server.use(express.json())
 
 server.use(`/api/posts`, postsRoutes)
 server.use(`/`, (req, res) => res.send(`Welcome to the POSTS API server`))
